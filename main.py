b7c208c2484982e21cfbd5755881295ead800d11
Abhishek Sharma <abhishek.iitjmu@gmail.com>
2021-04-20 23:38:28 +0530

    Feature add: PAUSE change file: main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pygame   
-from checkers.constants import  SQUARE_SIZE, WIDTH, HEIGHT
+from checkers.constants import  SQUARE_SIZE, WIDTH, HEIGHT,BLACK,WHITE
 from checkers.board import Board
 from checkers.game import Game
 
 class Main:
+    pygame.font.init()
+    font = pygame.font.Font('freesansbold.ttf',25)
+    text = font.render('GAME IS PAUSED! PRESS SPACE TO RESUME',True,BLACK,WHITE)
+    textshow = text.get_rect()
+    textshow.center=(WIDTH//2,HEIGHT//2)
+
     def __init__(self, WIN):
         self.WIN = WIN
         self.FPS = 60
@@ -14,6 +20,19 @@ class Main:
         col = x // SQUARE_SIZE
         return row, col
 
+    def Pause(self):
+        pause = 1
+        self.WIN.blit(self.text,self.textshow)
+        while pause > 0:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pause = -1
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        pause = 0
+            pygame.display.update()
+        return pause  
+
     def START_GAME(self):
         run = True
         clock = pygame.time.Clock()
@@ -21,6 +40,11 @@ class Main:
         while run:
             clock.tick(self.FPS)
             for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key==pygame.K_SPACE:
+                        status = self.Pause()
+                        if(status == -1):
+                            run = False
                 if event.type == pygame.QUIT:
                     run = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
